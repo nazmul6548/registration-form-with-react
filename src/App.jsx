e36ca1814f816a5c647f90bc03615dece844fe6d
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-18 13:05:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Registration from './Components/Registration/Registration'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Registration></Registration>
+    </div>
+  )
+}
+
+export default App
